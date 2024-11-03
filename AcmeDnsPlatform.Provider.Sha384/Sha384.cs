b61b099d3d6e716bcfb55f8c8031ecbb016b67be
Joel Strasser <strasser999@gmail.com>
2024-11-03 18:09:25 +0100

    feat: add Sha384 hash function

---
+++ AcmeDnsPlatform.Provider.Sha384/Sha384.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+using AcmeDnsPlatform.Api;
+
+namespace AcmeDnsPlatform.Provider.Sha384Provider;
+
+public class Sha384 : IHashFunction
+{
+    public byte[] Hash(byte[] bytes)
+    {
+        return SHA3_384.HashData(bytes);
+    }
+}
